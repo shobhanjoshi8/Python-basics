9523a80bf5679ab9e12906b7c9793729e2130e04
shobhanjoshi8 <78548448+shobhanjoshi8@users.noreply.github.com>
2021-02-09 10:56:55 +0530

    quadratic equation
    when two roots are known then find the quadratic equation

---
+++ quad.py
@@ -0,0 +1,8 @@
+#when root are known
+a = input("enter the first root:")
+b = input("enter the second root:")
+ai = int(a)
+bi = int(b)
+sum = ai + bi
+product = ai * bi
+print("x^2 - (", sum, "x) + (", product, ") = 0")
